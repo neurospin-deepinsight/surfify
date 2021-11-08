0f13bad9c9229f54f2b7f7290a530604f7cd5080
CorentinAmbroise <corentin.ambroise9132@gmail.com>
2021-11-08 17:40:43 +0100

    splitted SpericalGVAE in encoder and decoder, so we can give it different encoders and decoders

--- surfify/losses/vae.py
+++ surfify/losses/vae.py
@@ -17,10 +17,30 @@ from torch.nn import functional as func
 from torch.distributions import Normal, kl_divergence
 
 
+def img_log_likelihood(recon, xs):
+    """ Computes the log likelihood of the input image given the reconstructed
+    image for each subject
+
+    Parameters
+    ----------
+    recon: Tensor (N, C, H, W)
+        reconstructed images
+    xs: Tensor (N, C, H, W)
+        original images
+
+    Returns
+    -------
+    log_likelihoods: Tensor (N)
+        log likelihood for each image
+    """
+    return -Normal(recon, torch.ones_like(recon)).log_prob(xs).sum(
+        dim=(1, 2, 3))
+
+
 class SphericalVAELoss(object):
     """ Spherical VAE Loss.
     """
-    def __init__(self, beta=9, left_mask=None, right_mask=None):
+    def __init__(self, beta=9, left_mask=None, right_mask=None, use_mse=True):
         """ Init class.
 
         Parameters
@@ -31,12 +51,15 @@ class SphericalVAELoss(object):
             left cortical binary mask.
         right_mask: Tensor (azimuth, elevation), default None
             right cortical binary mask.
+        use_mse: bool, default True
+            optionally uses the log likelihood.
         """
         super(SphericalVAELoss, self).__init__()
         self.beta = beta
         self.left_mask = left_mask
         self.right_mask = right_mask
         self.layer_outputs = None
+        self.use_mse = use_mse
 
     def __call__(self, left_recon_x, right_recon_x, left_x, right_x):
         """ Compute loss.
@@ -54,19 +77,28 @@ class SphericalVAELoss(object):
             self.right_mask = torch.ones(
                 (right_x.shape[-2], right_x.shape[-1]), dtype=int).to(device)
 
-        # Reconstruction loss term i.e. the mean squared error
-        left_mse = func.mse_loss(
-            left_recon_x * self.left_mask.detach(),
-            left_x * self.left_mask.detach(), reduction="mean")
-        right_mse = func.mse_loss(
-            right_recon_x * self.right_mask.detach(),
-            right_x * self.right_mask.detach(), reduction="mean")
+        # Reconstruction loss terms
+        if self.use_mse:
+            left_recon_loss = func.mse_loss(
+                left_recon_x * self.left_mask.detach(),
+                left_x * self.left_mask.detach(), reduction="mean")
+            right_recon_loss = func.mse_loss(
+                right_recon_x * self.right_mask.detach(),
+                right_x * self.right_mask.detach(), reduction="mean")
+        else:
+            left_recon_loss = img_log_likelihood(
+                left_recon_x * self.left_mask.detach(),
+                left_x * self.left_mask.detach()).mean()
+            right_recon_loss = img_log_likelihood(
+                right_recon_x * self.right_mask.detach(),
+                right_x * self.right_mask.detach()).mean()
 
         # Latent loss between approximate posterior and prior for z
         kl_div = kl_divergence(q, Normal(0, 1)).mean(dim=0).sum()
 
         # Need to maximise the ELBO with respect to these weights
-        loss = left_mse + right_mse + self.beta * kl_div
+        loss = left_recon_loss + right_recon_loss + self.beta * kl_div
 
-        return loss, {"left_mse": left_mse, "right_mse": right_mse,
-                      "kl_div": kl_div}
+        return loss, {"left_recon_loss": left_recon_loss,
+                      "right_recon_loss": right_recon_loss,
+                      "kl_div": kl_div, "beta": self.beta}
--- surfify/models/vae.py
+++ surfify/models/vae.py
@@ -325,7 +325,9 @@ class SphericalGVAE(nn.Module):
     >>> print(out[0].shape, out[1].shape)
     """
     def __init__(self, input_channels=1, input_dim=192, latent_dim=64,
-                 conv_flts=[64, 128, 128, 256, 256], fusion_level=1):
+                 conv_flts=[64, 128, 128, 256, 256], fusion_level=1,
+                 activation="LeakyReLU", batch_norm=False,
+                 encoder=None, decoder=None):
         """ Init class.
 
         Parameters
@@ -341,6 +343,17 @@ class SphericalGVAE(nn.Module):
         fusion_level: int, default 1
             at which max pooling level left and right hemisphere data
             are concatenated.
+        activation: str, default 'LeakyReLU'
+            activation function's class name in pytorch's nn module to use
+            after each convolution
+        batch_norm: bool, default False
+            optionally uses batch normalization after each convolution
+        encoder: nn.Module or None, default None
+            encoder model to use. If None instantiate a HemiFusionEncoder
+            with the provided parameters.
+        decoder: nn.Module or None, default None
+            decoder model to use. If None instantiate a HemiFusionDecoder
+            with the provided parameters.
         """
         logger.debug("SphericalGVAE init...")
         super(SphericalGVAE, self).__init__()
@@ -349,92 +362,16 @@ class SphericalGVAE(nn.Module):
         self.latent_dim = latent_dim
         self.conv_flts = conv_flts
         self.n_layers = len(self.conv_flts)
-        self.top_flatten_dim = int(self.input_dim / (2 ** self.n_layers))
-        self.top_final = self.conv_flts[-1] * self.top_flatten_dim ** 2
         if fusion_level > self.n_layers or fusion_level <= 0:
             raise ValueError("Impossible to use input fusion level with "
                              "'{0}' layers.".format(self.n_layers))
         self.fusion_level = fusion_level
-
-        # define the encoder
-        self.enc_left_conv = nn.Sequential()
-        self.enc_right_conv = nn.Sequential()
-        self.enc_w_conv = nn.Sequential()
-        multi_path = True
-        input_channels = self.input_channels
-        for idx in range(self.n_layers):
-            if idx == self.fusion_level:
-                multi_path = False
-                input_channels *= 2
-            if multi_path:
-                output_channels = int(self.conv_flts[idx] / 2)
-                if idx == 0:
-                    kernel_size = 8
-                    pad = 3
-                else:
-                    kernel_size = 4
-                    pad = 1
-                lconv = IcoSpMaConv(
-                    in_feats=input_channels, out_feats=output_channels,
-                    kernel_size=kernel_size, stride=2, pad=pad)
-                self.enc_left_conv.add_module("l_enc_{0}".format(idx), lconv)
-                rconv = IcoSpMaConv(
-                    in_feats=input_channels, out_feats=output_channels,
-                    kernel_size=kernel_size, stride=2, pad=pad)
-                self.enc_right_conv.add_module("r_enc_{0}".format(idx), rconv)
-                input_channels = output_channels
-            else:
-                conv = IcoSpMaConv(
-                    input_channels, self.conv_flts[idx], kernel_size=4,
-                    stride=2, pad=1)
-                self.enc_w_conv.add_module("enc_{0}".format(idx), conv)
-                input_channels = self.conv_flts[idx]
-        self.enc_w_dense = nn.Linear(self.top_final, self.latent_dim * 2)
-
-        # define the decoder
-        self.dec_w_dense = nn.Linear(self.latent_dim, self.top_final)
-        self.dec_w_conv = nn.Sequential()
-        self.dec_left_conv = nn.Sequential()
-        self.dec_right_conv = nn.Sequential()
-        input_channels = self.conv_flts[self.n_layers - 1]
-        if self.fusion_level == self.n_layers:
-            multi_path = True
-            input_channels = int(input_channels / 2)
-        else:
-            multi_path = False
-        for idx in range(self.n_layers - 1, -1, -1):
-            if multi_path:
-                if idx == 0:
-                    kernel_size = 8
-                    pad = 3
-                    zero_pad = 9
-                    output_channels = self.input_channels
-                else:
-                    kernel_size = 4
-                    pad = 1
-                    zero_pad = 3
-                    output_channels = int(self.conv_flts[idx - 1] / 2)
-                lconv = IcoSpMaConvTranspose(
-                    in_feats=input_channels, out_feats=output_channels,
-                    kernel_size=kernel_size, stride=2, pad=pad,
-                    zero_pad=zero_pad)
-                self.dec_left_conv.add_module("l_dec_{0}".format(idx), lconv)
-                rconv = IcoSpMaConvTranspose(
-                    in_feats=input_channels, out_feats=output_channels,
-                    kernel_size=kernel_size, stride=2, pad=pad,
-                    zero_pad=zero_pad)
-                self.dec_right_conv.add_module("r_dec_{0}".format(idx), rconv)
-                input_channels = output_channels
-            else:
-                conv = IcoSpMaConvTranspose(
-                    in_feats=input_channels, out_feats=self.conv_flts[idx - 1],
-                    kernel_size=4, stride=2, pad=1, zero_pad=3)
-                self.dec_w_conv.add_module("dec_{0}".format(idx), conv)
-                input_channels = self.conv_flts[idx - 1]
-            if idx == self.fusion_level:
-                multi_path = True
-                input_channels = int(input_channels / 2)
-        self.relu = nn.ReLU(inplace=True)
+        self.encoder = encoder or HemiFusionEncoder(
+            input_channels, input_dim, latent_dim, conv_flts,
+            fusion_level, activation, batch_norm)
+        self.decoder = decoder or HemiFusionDecoder(
+            input_channels, self.encoder.output_dim, latent_dim, conv_flts,
+            fusion_level, activation, batch_norm)
 
     def encode(self, left_x, right_x):
         """ The encoder.
@@ -451,16 +388,7 @@ class SphericalGVAE(nn.Module):
         q(z | x): Normal (batch_size, <latent_dim>)
             a Normal distribution.
         """
-        left_x = self.enc_left_conv(left_x)
-        right_x = self.enc_right_conv(right_x)
-        x = torch.cat((left_x, right_x), dim=1)
-        x = self.relu(x)
-        for mod in self.enc_w_conv.children():
-            x = self.relu(mod(x))
-        x = x.view(-1, self.top_final)
-        x = self.enc_w_dense(x)
-        z_mu, z_logvar = torch.chunk(x, chunks=2, dim=1)
-        return Normal(loc=z_mu, scale=z_logvar.exp().pow(0.5))
+        return self.encoder((left_x, right_x))
 
     def decode(self, z):
         """ The decoder.
@@ -477,15 +405,7 @@ class SphericalGVAE(nn.Module):
         right_recon_x: Tensor (samples, <input_channels>, azimuth, elevation)
             reconstructed right cortical texture.
         """
-        x = self.relu(self.dec_w_dense(z))
-        x = x.view(-1, self.conv_flts[-1], self.top_flatten_dim,
-                   self.top_flatten_dim)
-        for mod in self.dec_w_conv.children():
-            x = self.relu(mod(x))
-        left_recon_x, right_recon_x = torch.chunk(x, chunks=2, dim=1)
-        left_recon_x = self.dec_left_conv(left_recon_x)
-        right_recon_x = self.dec_right_conv(right_recon_x)
-        return left_recon_x, right_recon_x
+        return self.decoder(z)
 
     def reparameterize(self, q):
         """ Implement the reparametrization trick.
@@ -525,3 +445,264 @@ class SphericalGVAE(nn.Module):
         logger.debug(debug_msg("left recon cortical", left_recon_x))
         logger.debug(debug_msg("right recon cortical", right_recon_x))
         return left_recon_x, right_recon_x, {"q": q, "z": z}
+
+
+def compute_output_dim(input_dim, convnet):
+    """ Compute the output dimension of a convolutional network
+    that takes as input a square input (H = W)
+
+    Parameters
+    ----------
+    input_dim: int
+        input height and weight
+    convnet: iterable[nn.Module]
+        iterable containing the various layers. For now, the function
+        can only work with nn.Conv2d and IcoSpMaConv layers
+
+    Returns
+    -------
+    output_dim: int
+        output dimension
+    """
+    output_dim = input_dim
+    for layer in convnet:
+        if type(layer) is nn.Conv2d:
+            output_dim = int(
+                (output_dim + 2 * layer.padding[0] - layer.dilation[0] *
+                 (layer.kernel_size[0] - 1) - 1) / layer.stride[0] + 1)
+        elif type(layer) is IcoSpMaConv:
+            output_dim = compute_output_dim(
+                output_dim + 2 * layer.pad, [layer.conv])
+    return output_dim
+
+
+class HemiFusionEncoder(nn.Module):
+    def __init__(self, input_channels, input_dim, latent_dim,
+                 conv_flts=[64, 128, 128, 256, 256], fusion_level=1,
+                 activation="LeakyReLU", batch_norm=False):
+        """ Init class.
+
+        Parameters
+        ----------
+        input_channels: int, default 1
+            the number of input channels.
+        input_dim: int, default 192
+            the size of the converted 3-D surface to the 2-D grid.
+        latent_dim: int, default 64
+            the size of the latent space it encodes to.
+        conv_flts: list of int
+            the size of convolutional filters.
+        fusion_level: int, default 1
+            at which max pooling level left and right hemisphere data
+            are concatenated.
+        activation: str, default 'LeakyReLU'
+            activation function's class name in pytorch's nn module to use
+            after each convolution
+        batch_norm: bool, default False
+            optionally uses batch normalization after each convolution
+        """
+        logger.debug("HemiFusionEncoder init...")
+        super().__init__()
+        self.input_channels = input_channels
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.conv_flts = conv_flts
+        self.n_layers = len(self.conv_flts)
+        if fusion_level > self.n_layers or fusion_level <= 0:
+            raise ValueError("Impossible to use input fusion level with "
+                             "'{0}' layers.".format(self.n_layers))
+        self.fusion_level = fusion_level
+
+        self.left_conv = nn.Sequential()
+        self.right_conv = nn.Sequential()
+        self.w_conv = nn.Sequential()
+        input_channels = self.input_channels
+        for idx in range(self.n_layers):
+            if idx == 0:
+                kernel_size = 8
+                pad = 3
+            else:
+                kernel_size = 4
+                pad = 1
+            output_channels = self.conv_flts[idx]
+            if idx < fusion_level:
+                output_channels /= 2
+                lconv = IcoSpMaConv(
+                    in_feats=input_channels, out_feats=int(output_channels),
+                    kernel_size=kernel_size, stride=2, pad=pad)
+                self.left_conv.add_module("l_conv_{0}".format(idx), lconv)
+                if batch_norm:
+                    self.left_conv.add_module(
+                        "l_bn_{0}".format(idx),
+                        nn.BatchNorm2d(int(output_channels)))
+                self.left_conv.add_module(
+                    "l_act_{0}".format(idx),
+                    getattr(nn, activation)(inplace=True))
+                rconv = IcoSpMaConv(
+                    in_feats=input_channels, out_feats=int(output_channels),
+                    kernel_size=kernel_size, stride=2, pad=pad)
+                self.right_conv.add_module("r_conv_{0}".format(idx), rconv)
+                if batch_norm:
+                    self.right_conv.add_module(
+                        "r_bn_{0}".format(idx),
+                        nn.BatchNorm2d(int(output_channels)))
+                self.right_conv.add_module(
+                    "r_act_{0}".format(idx),
+                    getattr(nn, activation)(inplace=True))
+                input_channels = int(output_channels)
+            else:
+                input_channels = self.conv_flts[idx - 1]
+                conv = IcoSpMaConv(
+                    in_feats=input_channels, out_feats=self.conv_flts[idx],
+                    kernel_size=kernel_size, stride=2, pad=pad)
+                self.w_conv.add_module("conv_{0}".format(idx), conv)
+                if batch_norm:
+                    self.w_conv.add_module(
+                        "bn_{0}".format(idx),
+                        nn.BatchNorm2d(self.conv_flts[idx]))
+                self.w_conv.add_module("act_{0}".format(idx),
+                                       getattr(nn, activation)(inplace=True))
+
+        self.output_dim = compute_output_dim(input_dim,
+                                             [*self.left_conv, *self.w_conv])
+        self.flatten_dim = self.output_dim ** 2 * self.conv_flts[-1]
+        self.w_dense = nn.Linear(self.flatten_dim, self.latent_dim * 2)
+
+    def forward(self, x):
+        """ The encoder.
+
+        Parameters
+        ----------
+        left_x: Tensor (samples, <input_channels>, azimuth, elevation)
+            input left cortical texture.
+        right_x: Tensor (samples, <input_channels>, azimuth, elevation)
+            input right cortical texture.
+
+        Returns
+        -------
+        q(z | x): Normal (batch_size, <latent_dim>)
+            a Normal distribution.
+        """
+        left_x, right_x = x
+        left_x = self.left_conv(left_x)
+        right_x = self.right_conv(right_x)
+        x = torch.cat((left_x, right_x), dim=1)
+        x = self.w_conv(x)
+        x = x.view(-1, self.flatten_dim)
+        x = self.w_dense(x)
+        z_mu, z_logvar = torch.chunk(x, chunks=2, dim=1)
+        return Normal(loc=z_mu, scale=(z_logvar * 0.5).exp())
+
+
+class HemiFusionDecoder(nn.Module):
+    def __init__(self, output_channels, input_dim, latent_dim,
+                 conv_flts=[64, 128, 128, 256, 256], fusion_level=1,
+                 activation="LeakyReLU", batch_norm=False):
+        """ Init class.
+
+        Parameters
+        ----------
+        output_channels: int, default 1
+            the number of output channels.
+        input_dim: int,
+            the size of the squared input to the convnet, after the dense
+            layer transforming the input from the latent space.
+        latent_dim: int, default 64
+            the size of the latent space it decodes from.
+        conv_flts: list of int
+            the size of convolutional filters, given in reverse order: the
+            first filter in the list will be the last one in the network.
+        fusion_level: int, default 1
+            at which max pooling level left and right hemisphere data
+            are concatenated.
+        activation: str, default 'LeakyReLU'
+            activation function's class name in pytorch's nn module to use
+            after each convolution
+        batch_norm: bool, default False
+            optionally uses batch normalization after each convolution
+        """
+        logger.debug("HemiFusionDecoder init...")
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.conv_flts = conv_flts.copy()
+        self.n_layers = len(conv_flts)
+        self.conv_flts.insert(0, output_channels*2)
+        flatten_dim = input_dim ** 2 * conv_flts[-1]
+        self.w_dense = nn.Linear(latent_dim, flatten_dim)
+        self.w_conv = nn.Sequential()
+        self.left_conv = nn.Sequential()
+        self.right_conv = nn.Sequential()
+        self.fusion_level = fusion_level
+        for idx in range(self.n_layers, 0, -1):
+            if idx == 1:
+                kernel_size = 8
+                pad = 3
+                zero_pad = 9
+            else:
+                kernel_size = 4
+                pad = 1
+                zero_pad = 3
+            input_channels = self.conv_flts[idx]
+            output_channels = self.conv_flts[idx - 1]
+            if idx < fusion_level + 1:
+                input_channels = int(input_channels / 2)
+                output_channels = int(output_channels / 2)
+                lconv = IcoSpMaConvTranspose(
+                    in_feats=input_channels, out_feats=output_channels,
+                    kernel_size=kernel_size, stride=2, pad=pad,
+                    zero_pad=zero_pad)
+                if batch_norm:
+                    self.left_conv.add_module(
+                        "l_bn_{0}".format(idx),
+                        nn.BatchNorm2d(input_channels))
+                self.left_conv.add_module("l_act_{0}".format(idx),
+                                          getattr(nn, activation)())
+                self.left_conv.add_module("l_conv_{0}".format(idx), lconv)
+                rconv = IcoSpMaConvTranspose(
+                    in_feats=input_channels, out_feats=output_channels,
+                    kernel_size=kernel_size, stride=2, pad=pad,
+                    zero_pad=zero_pad)
+                if batch_norm:
+                    self.right_conv.add_module(
+                        "r_bn_{0}".format(idx),
+                        nn.BatchNorm2d(input_channels))
+                self.right_conv.add_module("r_act_{0}".format(idx),
+                                           getattr(nn, activation)())
+                self.right_conv.add_module("r_conv_{0}".format(idx), rconv)
+            else:
+                conv = IcoSpMaConvTranspose(
+                    input_channels, output_channels, kernel_size=kernel_size,
+                    stride=2, pad=pad, zero_pad=zero_pad)
+                if batch_norm and idx != self.n_layers:
+                    self.w_conv.add_module(
+                        "bn_{0}".format(idx),
+                        nn.BatchNorm2d(output_channels))
+                self.w_conv.add_module("act_{0}".format(idx),
+                                       getattr(nn, activation)(inplace=True))
+                self.w_conv.add_module("conv_{0}".format(idx), conv)
+
+    def forward(self, z):
+        """ The decoder.
+
+        Parameters
+        ----------
+        z: Tensor (samples, <latent_dim>)
+            the stochastic latent state z.
+
+        Returns
+        -------
+        left_recon_x: Tensor (samples, <input_channels>, azimuth, elevation)
+            reconstructed left cortical texture.
+        right_recon_x: Tensor (samples, <input_channels>, azimuth, elevation)
+            reconstructed right cortical texture.
+        """
+        x = self.w_dense(z)
+        x = x.view(-1, self.conv_flts[-1], self.input_dim,
+                   self.input_dim)
+        # if self.fusion_level < self.n_layers:
+        x = self.w_conv(x)
+        left_recon_x, right_recon_x = torch.chunk(x, chunks=2, dim=1)
+        left_recon_x = self.left_conv(left_recon_x)
+        right_recon_x = self.right_conv(right_recon_x)
+        return left_recon_x, right_recon_x
